b42249e6a45d4496d7fd9b9f412fa84e7576c9d9
Fred Lai <fdlai@yahoo.com>
2026-08-26 15:36:31 -0400

    starting shell of the homepage

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,2 +1,8 @@
 // Main dashboard for authenticated admins.
 // Provides administrative functionality such as user, staff, and account management.
+
+function AdminDashboard() {
+  return <h1>Admin Dashboard</h1>;
+}
+
+export default AdminDashboard;
