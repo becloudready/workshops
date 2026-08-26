b0e65bb902610f0ee61e2f33bcf48c7c644ce6ea
eman <eespiti@jacksonville.edu>
2026-08-26 14:37:22 -0400

    started admin

---
+++ src/features/admin/adminDashboard.jsx
@@ -0,0 +1,19 @@
+function AdminDashboard()
+{
+    return(
+        <div className="p-6">
+            <div className="mb-8">
+                <h1 className="text- 3x1 font bold">
+                    Admin Dashboard
+                </h1>
+
+                <p className="mt-2 text-gray-600">
+                    Manage users, Staff, and Accounts
+                </p>
+            </div>
+            
+        </div>
+
+    )
+
+}
