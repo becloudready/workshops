4633442ceb951eb6f0cf113cc9fae746d3e2604a
BrendanGeary2020 <94076430+BrendanGeary2020@users.noreply.github.com>
2026-08-28 14:10:58 -0400

    trainer side bar

--- workshops/fullstack-aws/projects/submission/Group3/frontend/task-board-frontend/src/store/store.js
+++ workshops/fullstack-aws/projects/submission/Group3/frontend/task-board-frontend/src/store/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import TraineeSlice from "./TraineeStore";
+import TrainerSlice from "./TrainerStore";
 
 export const Store = configureStore({
   reducer: {
     trainee: TraineeSlice,
+    trainer: TrainerSlice,
   },
 });
 
