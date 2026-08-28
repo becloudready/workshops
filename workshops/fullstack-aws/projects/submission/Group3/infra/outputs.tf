6acd854148629ad39a31fe25e3425d74df9b70ae
Connor Vandrush <connor.vandrush@outlook.com>
2026-08-28 18:25:47 -0400

    Deployed version

--- workshops/fullstack-aws/projects/submission/Group3/infra/outputs.tf
+++ workshops/fullstack-aws/projects/submission/Group3/infra/outputs.tf
@@ -2,3 +2,8 @@ output "api_endpoint" {
   description = "Base URL of the deployed API"
   value       = aws_apigatewayv2_stage.default.invoke_url
 }
+
+output "frontend_url" {
+  description = "URL of the deployed frontend (S3 static website endpoint)"
+  value       = aws_s3_bucket_website_configuration.frontend.website_endpoint
+}
